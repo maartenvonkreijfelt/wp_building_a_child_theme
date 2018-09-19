f1e4104c28983d6b41d165becf2beaea411458a1
Maarten von Kreijfelt <m.vonkreijfelt@preludio.nl>
2018-09-19 11:32:33 +0200

    update

--- popper-child/page-templates/page-sidebar-left.php
+++ popper-child/page-templates/page-sidebar-left.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Template Name: Sidebar right
+Template Name: Sidebar left
 */
 
 
--- popper-child/single.php
+++ popper-child/single.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The template for displaying all single posts in the cild theme.
+ * The Child Theme template for displaying all single posts.
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
  *
@@ -16,30 +16,25 @@ get_header(); ?>
 
 			<?php get_template_part( 'template-parts/content', 'single' ); ?>
 
-
-			<?php
-			// Previous/next post navigation.
-			the_post_navigation( array(
-				'next_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Next', 'popper' ) . '</span> ' .
-				               '<span class="screen-reader-text">' . __( 'Next post:', 'popper' ) . '</span> ' .
-				               '<span class="post-title">%title</span>',
-				'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Previous', 'popper' ) . '</span> ' .
-				               '<span class="screen-reader-text">' . __( 'Previous post:', 'popper' ) . '</span> ' .
-				               '<span class="post-title">%title</span>',
-			) );
-			?>
-
-
-
-
-
+            <?php
+            // Previous/next post navigation.
+            the_post_navigation( array(
+                'next_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Next', 'popper' ) . '</span> ' .
+                    '<span class="screen-reader-text">' . __( 'Next post:', 'popper' ) . '</span> ' .
+                    '<span class="post-title">%title</span>',
+                'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Previous', 'popper' ) . '</span> ' .
+                    '<span class="screen-reader-text">' . __( 'Previous post:', 'popper' ) . '</span> ' .
+                    '<span class="post-title">%title</span>',
+            ) );
+            ?>
+            
 			<?php
 				// If comments are open or we have at least one comment, load up the comment template.
 				if ( comments_open() || get_comments_number() ) :
 					comments_template();
 				endif;
 			?>
-			
+
 
 
 		<?php endwhile; // End of the loop. ?>
